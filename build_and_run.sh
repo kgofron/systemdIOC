0d22316d51ba28ae986af14fe970f299573bdd5f
K. Gofron <kgofron@comcast.net>
2025-06-08 05:55:58 -0400

    Build and run script

---
+++ build_and_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+echo "Building systemd_control..."
+g++ -std=c++17 -Wall -Wextra systemd_control.cpp -o systemd_control -lsystemd
+
+echo "Setting up permissions..."
+sudo chown root:root systemd_control
+sudo chmod u+s systemd_control
+
+echo "Running systemd_control..."
+./systemd_control
+
+echo "Done!" 
