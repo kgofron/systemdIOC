966d4fd5b9eb41c94f1fea132d42b28badf4a4d6
K. Gofron <kgofron@comcast.net>
2025-06-18 19:15:55 -0400

    robust reading of serval status

--- systemdIocApp/src/systemdDevSup.cpp
+++ systemdIocApp/src/systemdDevSup.cpp
@@ -33,17 +33,11 @@ static long read_stringin(void* prec) {
     sd_bus_message* reply = nullptr;
 
     // Get the unit path
-    ret = sd_bus_call_method(bus,
-                           "org.freedesktop.systemd1",
-                           "/org/freedesktop/systemd1",
-                           "org.freedesktop.systemd1.Manager",
-                           "GetUnit",
-                           &error,
-                           &reply,
-                           "s",
-                           "serval.service");
-
+    ret = sd_bus_call_method(bus, "org.freedesktop.systemd1", "/org/freedesktop/systemd1",
+                            "org.freedesktop.systemd1.Manager", "GetUnit",
+                            &error, &reply, "s", "serval.service");
     if (ret < 0) {
+        printf("GetUnit error: %s\n", error.message ? error.message : strerror(-ret));
         recGblSetSevr(psi, COMM_ALARM, INVALID_ALARM);
         sd_bus_error_free(&error);
         sd_bus_unref(bus);
@@ -53,45 +47,47 @@ static long read_stringin(void* prec) {
     const char* unit_path;
     ret = sd_bus_message_read(reply, "o", &unit_path);
     sd_bus_message_unref(reply);
-
+    sd_bus_error_free(&error);
     if (ret < 0) {
+        printf("Read unit path error\n");
         recGblSetSevr(psi, COMM_ALARM, INVALID_ALARM);
         sd_bus_unref(bus);
         return -1;
     }
 
     // Get the unit's ActiveState
-    ret = sd_bus_call_method(bus,
-                           "org.freedesktop.systemd1",
-                           unit_path,
-                           "org.freedesktop.DBus.Properties",
-                           "Get",
-                           &error,
-                           &reply,
-                           "ss",
-                           "org.freedesktop.systemd1.Unit",
-                           "ActiveState");
-
+    sd_bus_error state_error = SD_BUS_ERROR_NULL;
+    sd_bus_message* state_reply = nullptr;
+    ret = sd_bus_call_method(bus, "org.freedesktop.systemd1", unit_path,
+                            "org.freedesktop.DBus.Properties", "Get",
+                            &state_error, &state_reply, "ss",
+                            "org.freedesktop.systemd1.Unit", "ActiveState");
     if (ret < 0) {
+        printf("Get ActiveState error: %s\n", state_error.message ? state_error.message : strerror(-ret));
         recGblSetSevr(psi, COMM_ALARM, INVALID_ALARM);
-        sd_bus_error_free(&error);
+        sd_bus_error_free(&state_error);
         sd_bus_unref(bus);
         return -1;
     }
 
     const char* state;
-    ret = sd_bus_message_read(reply, "v", "s", &state);
-    sd_bus_message_unref(reply);
-
+    ret = sd_bus_message_read(state_reply, "v", "s", &state);
+    sd_bus_message_unref(state_reply);
+    sd_bus_error_free(&state_error);
     if (ret < 0) {
+        printf("Read state value error\n");
         recGblSetSevr(psi, COMM_ALARM, INVALID_ALARM);
         sd_bus_unref(bus);
         return -1;
     }
 
-    // Copy the state to the record's value
-    strncpy(psi->val, state, sizeof(psi->val) - 1);
-    psi->val[sizeof(psi->val) - 1] = '\0';
+    if (state && strlen(state) > 0) {
+        strncpy(psi->val, state, sizeof(psi->val) - 1);
+        psi->val[sizeof(psi->val) - 1] = '\0';
+    } else {
+        strncpy(psi->val, "unknown", sizeof(psi->val) - 1);
+        psi->val[sizeof(psi->val) - 1] = '\0';
+    }
 
     sd_bus_unref(bus);
     return 0;
